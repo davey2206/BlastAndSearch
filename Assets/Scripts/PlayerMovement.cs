a39a2577d4b0febc474926a90cbdc3622bea3274
Davopia <davey2206@live.nl>
2024-09-11 15:57:27 +0200

    ✨merge fixes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,24 +8,17 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] GameObject RHand;
     [SerializeField] float Speed = 2;
 
-    [SerializeField] TextMeshProUGUI text;
-
     bool move = false;
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 dir = RHand.transform.position - LHand.transform.position;
-        dir.Normalize();
-        dir = new Vector3(dir.x, 0, dir.z);
-
-        text.text = dir.ToString();
         
         if (move)
         {
-            //Vector3 dir = Hand.transform.position - Body.transform.position;
-            //dir.Normalize();
-            //dir = new Vector3(dir.x, Player.transform.position.y, dir.z);
+            Vector3 dir = RHand.transform.position - LHand.transform.position;
+            dir.Normalize();
+            dir = new Vector3(dir.x, 0, dir.z);
 
             Player.Move(dir * Time.deltaTime * Speed);
         }
